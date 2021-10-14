9342d499e9d7c25de640f4da6296085191eeab02
priscillahnalubega <nalubegapriscillah@gmail.com>
2021-10-14 11:54:02 +0300

    add new solution

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -76,9 +76,19 @@ const calculateNext = (state) => {
   return result;
 };
 
-const iterate = (state, iterations) => {};
+const iterate = (state, iterations) => {
+  const states = [state];
+  for (let i = 0; i< iterations; i++){
+    states.push(calculateNext(states[states.length - 1]));
+  }
+  return states;
 
-const main = (pattern, iterations) => {};
+};
+
+const main = (pattern, iterations) => {
+  const results = iterate(startPatterns[pattern],iterations);
+  results.forEach(r => console.log(printCells(r)));
+};
 
 const startPatterns = {
     rpentomino: [
